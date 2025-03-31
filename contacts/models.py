0155211581b2f30a2b335079632a3c6ec93628df
Eaby Thomas C <eabythomascu@gmail.com>
2025-03-31 14:02:28 +0530

    models and migrations fix

--- contacts/models.py
+++ contacts/models.py
@@ -7,7 +7,7 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=100,null=True)
     last_name = models.CharField(max_length=100,null=True)
     email = models.EmailField(unique=True)
-    phone = models.CharField(null=True, blank=True)
+    phone = models.CharField(max_length=50,null=True, blank=True)
     country = models.CharField(max_length=10,null=True, blank=True)
     location_id = models.CharField(max_length=50, null=True, blank=True)
     type = models.CharField(max_length=20, choices=[("lead", "Lead"), ("customer", "Customer")],null=True, blank=True)
